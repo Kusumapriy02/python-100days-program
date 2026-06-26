3ab2226915a230bd45d0c1d1c95547c237eec479
Kusumapriy02 <113963110+Kusumapriy02@users.noreply.github.com>
2026-06-26 12:45:25 +0530

    Add files via upload
    learned python Tuples ,including tuple ,creation ,indexing ,slicing ,immutable , packing and unpacking ,(count() and index()).practiced examples to understand tuple operation and their real word usage

---
+++ tuple.py
@@ -0,0 +1,18 @@
+#tuple:tuple is a collection of different datatypes represent in() and seperated by,it is immutable
+waste = (1,2,3,"soo",[4,5],(23,24))
+print(waste)
+print(len(waste))
+#indexing in tuple
+print(waste.index("soo"))
+#methods:index(),count()
+#index:
+waste = (1,2,3,"soo",[4,5],(23,24))
+print(waste)
+print(len(waste))
+print(waste.index("soo"))
+#count():
+waste = (1,2,3,"soo",[4,5],(23,24))
+print(waste)
+print(len(waste))
+print(waste.count("soo"))
+
