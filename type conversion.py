1ac32733fdf852422648ba8e6ad556fbade6b4bc
Kusumapriy02 <113963110+Kusumapriy02@users.noreply.github.com>
2026-06-29 11:58:05 +0530

    type conversion
    Today I learned Type Conversion in Python, which is the process of converting a value from one data type to another. Understanding type conversion is essential for handling user input, performing calculations, and avoiding type-related errors in Python programs.

---
+++ type conversion.py
@@ -0,0 +1,52 @@
+#type conversion: This is process of converting one data  type to another..
+#in built fun:
+#str(),float(),int(),list(),dict()
+#int to string:str() for float:float()
+num = 34
+num_2 = float(num)
+print(num_2)
+print(type(num))
+so =  str(num)
+print(type(so))
+#flaoat:string , integer
+num = 8.6
+how = str(num)
+print(how)
+print(type(how))
+print(how)
+num_2 = int(num)
+print(num_2)
+#string : integer,float,list ,tuple
+hai = "789"
+num = int(hai)
+print(num + 23)
+#float
+hii = "7.8"
+num_2 = float(hii)
+print(num_2)
+#list:
+any_ = "3567"
+con_ = list(any_)
+print(con_)
+con_2 = tuple(any_)
+print(con_2)
+#list :string, tuple ,dict
+var_ =['p','y','t','h','o','n']
+text_ = "".join(var_)
+print(text_)
+#tuple:
+var_ =['p','y','t','h','o','n']
+some = tuple(var_)
+print(some)
+#dict:
+pyth_ = [('a',1),('b',8)]
+conver = dict(pyth_)
+print(conver)
+#tuple:list,string
+fam = (91,2,34)
+ntg = list(fam)
+print(ntg)
+#string:
+fam = ('h','e','l','l','o')
+text2_ = "".join(fam)
+print(text2_)
