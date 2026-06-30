d01c554c2d3d39d8ffa2564c337e3f39bee3e6cc
Kusumapriy02 <113963110+Kusumapriy02@users.noreply.github.com>
2026-06-30 12:10:41 +0530

    Add files via upload

---
+++ operators.py.py
@@ -0,0 +1,83 @@
+a = 20
+b =19
+print(a + b)
+
+
+a = 20
+b =19
+print(a - b)
+
+
+a = 20
+b =19
+print(a * b)
+
+
+
+a = 20
+b =19
+print(a / b)
+
+a = 20
+b =19
+print(a % b)
+
+a = 20
+b =19
+print(a ** b)
+
+#assignment operator
+a = 20
+a +=4
+print(a)
+
+a = 20
+a -=4
+print(a)
+
+a = 20
+a *=4
+print(a)
+
+a = 20
+a /=4
+print(a)
+
+#comparison operator:==,!=,<=,>=
+a = 20
+b = 7
+print(a == b)
+print(a != b)
+print(a <= b)
+print(a >= b)
+
+#logical oprator: and or not
+a = 20
+b = 7
+c = 89
+print(a >= b and b >=c)
+print(a >= b or  b >=c)
+
+#identity operator--is -->same object or not
+                    #is not-->
+a = [1,2,3]
+b = a
+print(a is b)
+
+
+a = [1,2,3]
+b = [1,2,3]
+print(a is not b)
+
+#membership operator:in , not in
+num = [1,2,3]
+print(1 in num)
+print(1 not in num)
+
+
+
+
+
+                        
+
+
